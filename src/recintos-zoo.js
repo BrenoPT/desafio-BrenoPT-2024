69633a18f971213c13d94f22cccbe1170d4ddc3e
Breno Paim Targa <brenotarga@gmail.com>
2024-09-23 19:38:46 -0300

    Comentários removidos.

--- src/recintos-zoo.js
+++ src/recintos-zoo.js
@@ -24,42 +24,33 @@ class RecintosZoo {
         animal = normalizaEntrada(animal)
 
         function validaEntrada(animal, quantidade) {
-            //animal tem que corresponder a uma key valida em animais
             if (!animais[animal]) {
                 return { erro: "Animal inválido" };
             }
-            //quantidade tem que ser um numero inteiro maior que 0
             if (typeof quantidade !== 'number' || quantidade < 1 || !Number.isInteger(quantidade)) {
                 return { erro: "Quantidade inválida" };
-            }
+            }y
         }
         function encontraRecintosViaveis() {
             recintos.forEach((recinto) => {
-                //itera sobre cada recinto da array recintos e testa se algum recinto tem o mesmo bioma que o bioma do animal requerido
+
                 const biomaCombina = recinto.bioma.some((bioma) => animais[animal].bioma.includes(bioma))
                 if (!biomaCombina) return;
 
-                //desconstroi o par [key: value] do objeto 'animaisPresentes' do recinto atual (ex: ['MACACO', 3] = {MACACO: 3}
                 const animaisPresentes = Object.entries(recinto.animaisPresentes)
 
-                //calcula o espaço já ocupado. O acumulador começa em 0, o método desconstrói a array criada em animaisPresentes e atribui
-                //estes valores aos parametros [animal, qnt], que são utilizados para obter o tamanho do animal na array animais e a quantidade
-                //deste animal no recinto para calcular o total já ocupado, este valor será usado posteriormente para validar o recinto por espaço
                 const espacoOcupado = animaisPresentes.reduce((acumulador, [animal, qnt]) => {
                     return acumulador + (animais[animal].tamanho * qnt)
                 }, 0)
 
-                //animais carnivoros só podem viver com a mesma espécie
                 if (animais[animal].carnivoro && animaisPresentes.length > 0) {
                     let apenasMesmaEspecie = animaisPresentes.every(([especie]) => especie === animal)
                     if (!apenasMesmaEspecie) return;
                 }
 
-                //checa se os animais podem coexistir
                 if (animaisPresentes.length > 0 && !podemCoexistir(recinto, animal)) return;
 
                 let espacoLivre;
-                //calcula espaço livre restante
                 let apenasMesmaEspecie = animaisPresentes.every(([especie]) => especie === animal)
                 if (apenasMesmaEspecie) {
                     espacoLivre = recinto.tamanho - (espacoOcupado + (animais[animal].tamanho * quantidade))
@@ -71,7 +62,6 @@ class RecintosZoo {
             })
         }
 
-        //esta helper function checa se o novo animal se sentira satisfeito no recinto baseado nos animais já presentes
         function podemCoexistir(recinto, animal) {
             const animaisPresentes = Object.entries(recinto.animaisPresentes);
             if (animaisPresentes.length === 0) {
@@ -93,8 +83,8 @@ class RecintosZoo {
         function normalizaEntrada(str) {
             return str
                 .normalize('NFD')
-                .replace(/[\u0300-\u036f]/g, '') //remove acentos
-                .toUpperCase(); //coloca em caixa alta
+                .replace(/[\u0300-\u036f]/g, '')
+                .toUpperCase(); 
         }
 
         const erro = validaEntrada(animal, quantidade)
